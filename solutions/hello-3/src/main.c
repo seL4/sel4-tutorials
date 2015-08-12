7580bf4923489ca731f7c58d902bc6dcedda3b72
Ihor Kuz <ihor.kuz@nicta.com.au>
2015-08-12 16:56:44 -0400

    Cleaned up hello-3 app.

--- solutions/hello-3/src/main.c
+++ solutions/hello-3/src/main.c
@@ -9,9 +9,8 @@
  */
 
 /*
- * Part X: IPC between 2 threads
+ * seL4 tutorial part 3: IPC between 2 threads
  */
-
 
 /* Include Kconfig variables. */
 #include <autoconf.h>
@@ -27,7 +26,6 @@
 
 #include <vka/object.h>
 #include <vka/object_capops.h>
-//#include <vka/vka.h>
 
 #include <allocman/allocman.h>
 #include <allocman/bootstrap.h>
@@ -38,18 +36,21 @@
 #include <sel4utils/vspace.h>
 #include <sel4utils/mapping.h>
 
-#define IPCBUF_FRAME_SIZE_BITS 12
-#define IPCBUF_VADDR 0x7000000
 
-#define EP_BADGE 0x61 // arbitrary (but unique) number
+/* constants */
+#define IPCBUF_FRAME_SIZE_BITS 12 // use a 4K frame for the IPC buffer
+#define IPCBUF_VADDR 0x7000000 // arbitrary (but free) address for IPC buffer
 
+#define EP_BADGE 0x61 // arbitrary (but unique) number for a badge
 #define MSG_DATA 0x6161 // arbitrary data to send
 
+/* global environment variables */
 seL4_BootInfo *info;
 simple_t simple;
 vka_t vka;
 allocman_t *allocman;
 
+/* variables shared with second thread */
 vka_object_t ep_object;
 cspacepath_t ep_cap_path;
 
@@ -57,170 +58,176 @@ cspacepath_t ep_cap_path;
 #define ALLOCATOR_STATIC_POOL_SIZE ((1 << seL4_PageBits) * 10)
 UNUSED static char allocator_mem_pool[ALLOCATOR_STATIC_POOL_SIZE];
 
+/* stack for the new thread */
 #define THREAD_2_STACK_SIZE 512
 static uint64_t thread_2_stack[THREAD_2_STACK_SIZE];
 
-void abort(void) {
-    while (1);
-}
+/* convenience function */
+extern void name_thread(seL4_CPtr tcb, char *name);
 
-void __arch_putchar(int c) {
-#ifdef CONFIG_DEBUG_BUILD
-    seL4_DebugPutChar(c);
-#endif
-}
-
+/* function to run in the new thread */
 void thread_2(void) {
-    UNUSED seL4_Word sender_badge;
-    UNUSED seL4_MessageInfo_t tag;
-    UNUSED seL4_Word msg;
+    seL4_Word sender_badge;
+    seL4_MessageInfo_t tag;
+    seL4_Word msg;
 
     printf("thread_2: hallo wereld\n");
 
+    /* wait for a message to come in over the endpoint */
     tag = seL4_Wait(ep_object.cptr, &sender_badge);
-    assert(sender_badge == EP_BADGE);
 
+    /* make sure it is what we expected */
+    assert(sender_badge == EP_BADGE);
     assert(seL4_MessageInfo_get_length(tag) == 1);
-    msg = seL4_GetMR(0); 
+
+    /* get the message stored in the first message register */
+    msg = seL4_GetMR(0);
     printf("thread_2: got a message %#x from %#x\n", msg, sender_badge);
 
+    /* modify the message and send it back */
     seL4_SetMR(0, ~msg);
     seL4_ReplyWait(ep_object.cptr, tag, &sender_badge);
-
-    //while (1);
 }
 
 int main(void)
 {
     UNUSED int error;
-    UNUSED void *ipcbuf_addr;
-    UNUSED seL4_CPtr ipc_frame;
 
-#ifdef SEL4_DEBUG_KERNEL
-    seL4_DebugNameThread(seL4_CapInitThreadTCB, "hello-libs-2");
-#endif
+    /* give us a name: useful for debugging if the thread faults */
+    name_thread(seL4_CapInitThreadTCB, "hello-3");
 
     /* get boot info */
     info = seL4_GetBootInfo();
 
-    printf("thread_2: %p\n", &thread_2);
-
     /* init simple */
     simple_default_init_bootinfo(&simple, info);
 
-    /* Print bootinfo and other info about simple */
+    /* print out bootinfo and other info about simple */
     simple_print(&simple);
 
     /* create an allocator */
-    allocman = bootstrap_use_current_simple(&simple, ALLOCATOR_STATIC_POOL_SIZE, allocator_mem_pool);
+    allocman = bootstrap_use_current_simple(&simple, ALLOCATOR_STATIC_POOL_SIZE,        allocator_mem_pool);
     assert(allocman);
 
     /* create a vka (interface for interacting with the underlying allocator) */
     allocman_make_vka(&vka, allocman);
 
-    /* get our cspace */
+    /* get our cspace root cnode */
     seL4_CPtr cspace_cap;
     cspace_cap = simple_get_cnode(&simple);
 
-    /* get our vspace root */
+    /* get our vspace root page directory */
     seL4_CPtr pd_cap;
     pd_cap = simple_get_pd(&simple);
 
-    /* create a TCB */
-    vka_object_t tcb_object;
-    tcb_object.cptr = 0; // compiler complains otherwise!
+    /* create a new TCB */
+    vka_object_t tcb_object = {0};
     error = vka_alloc_tcb(&vka, &tcb_object);
     assert(error == 0);
 
-    /* create and map an ipc buffer */
-    seL4_Word ipc_buffer_vaddr;
-    ipc_buffer_vaddr = IPCBUF_VADDR;
+    /*
+     * create and map an ipc buffer:
+     */
 
-    // get a frame cap
+    /* get a frame cap for the ipc buffer */
     vka_object_t ipc_frame_object;
     error = vka_alloc_frame(&vka, IPCBUF_FRAME_SIZE_BITS, &ipc_frame_object);
     assert(error == 0);
 
-    // map it
+    /*
+     * map the frame into the vspace at ipc_buffer_vaddr.
+     * To do this we first try to map it in to the root page directory.
+     * If there is already a page table mapped in the appropriate slot in the
+     * page diretory where we can insert this frame, then this will succeed.
+     * Otherwise we first need to create a page table, and map it in to
+     * the page`directory, before we can map the frame in.
+     */
+    seL4_Word ipc_buffer_vaddr;
+    ipc_buffer_vaddr = IPCBUF_VADDR;
     error = seL4_ARCH_Page_Map(ipc_frame_object.cptr, pd_cap, ipc_buffer_vaddr,
-                               seL4_AllRights, seL4_ARCH_Default_VMAttributes);
+        seL4_AllRights, seL4_ARCH_Default_VMAttributes);
     if (error != 0) {
+        /* create and map a page table */
         vka_object_t pt_object;
         error =  vka_alloc_page_table(&vka, &pt_object);
         assert(error == 0);
 
-    	error = seL4_ARCH_PageTable_Map(pt_object.cptr, pd_cap, 
-                                        ipc_buffer_vaddr,
-                                        seL4_ARCH_Default_VMAttributes);
+    	error = seL4_ARCH_PageTable_Map(pt_object.cptr, pd_cap,
+            ipc_buffer_vaddr, seL4_ARCH_Default_VMAttributes);
         assert(error == 0);
 
-        error = seL4_ARCH_Page_Map(ipc_frame_object.cptr, pd_cap, 
-                                   ipc_buffer_vaddr,
-                                   seL4_AllRights, 
-                                   seL4_ARCH_Default_VMAttributes);
+        /* then map the frame in */
+        error = seL4_ARCH_Page_Map(ipc_frame_object.cptr, pd_cap,
+            ipc_buffer_vaddr, seL4_AllRights, seL4_ARCH_Default_VMAttributes);
         assert(error == 0);
     }
 
+    /* set the IPC buffer's virtual address in a field of the IPC buffer */
     seL4_IPCBuffer *ipcbuf = (seL4_IPCBuffer*)ipc_buffer_vaddr;
     ipcbuf->userData = ipc_buffer_vaddr;
 
-    /* Create an endpoint */
-    
-    // create the object
+    /* create an endpoint */
     error = vka_alloc_endpoint(&vka, &ep_object);
     assert(error == 0);
 
-    // make a copy of it
-    vka_mint_object(&vka, &ep_object, &ep_cap_path, seL4_AllRights, seL4_CapData_Badge_new(EP_BADGE));
- 
-    seL4_CapData_t cspace_data;
-    cspace_data = seL4_CapData_Guard_new(0 /* guard bits */, 32 - info->initThreadCNodeSizeBits);
+    /* make a badged copy of it in our cspace.
+     * This copy will be used to send an IPC message to the original cap
+     */
+    vka_mint_object(&vka, &ep_object, &ep_cap_path, seL4_AllRights,
+        seL4_CapData_Badge_new(EP_BADGE));
 
-    /* initialise the TCB */
-    error = seL4_TCB_Configure(tcb_object.cptr /* service */, 
-                               seL4_CapNull /* fault ep */, 
-                               seL4_MaxPrio /* priority */,
-                               cspace_cap /* cspace root */, 
-                               cspace_data /* cspace root data */, 
-                               pd_cap /* vspace root */,
-                               seL4_NilData /* vspace root data */, 
-                               ipc_buffer_vaddr /* buffer */, 
-                               ipc_frame_object.cptr /* buffer frame */);
+    /* initialise the new TCB */
+    error = seL4_TCB_Configure(tcb_object.cptr, seL4_CapNull, seL4_MaxPrio,
+        cspace_cap, seL4_NilData, pd_cap, seL4_NilData,
+        ipc_buffer_vaddr, ipc_frame_object.cptr);
     assert(error == 0);
 
-#ifdef SEL4_DEBUG_KERNEL
-    seL4_DebugNameThread(tcb_object.cptr, "hello-libs-3: thread_2");
-#endif
+    /* give the new thread a name */
+    name_thread(tcb_object.cptr, "hello-3: thread_2");
 
+    /* set start up registers for the new thread */
     seL4_UserContext regs = {0};
     size_t regs_size = sizeof(seL4_UserContext) / sizeof(seL4_Word);
+
+    /* set instruction pointer where the thread shoud start running */
     sel4utils_set_instruction_pointer(&regs, (seL4_Word)thread_2);
 
     /* check that stack is aligned correctly */
     assert((seL4_Word) thread_2_stack % (sizeof(seL4_Word) * 2) == 0);
 
-    sel4utils_set_stack_pointer(&regs, (seL4_Word)(thread_2_stack + sizeof(thread_2_stack)));
+    /* set stack pointer for the new thread. remember the stack grows down */
+    sel4utils_set_stack_pointer(&regs, (seL4_Word)(thread_2_stack +
+        sizeof(thread_2_stack)));
 
+    /* set the gs register for thread local storage */
     regs.gs = IPCBUF_GDT_SELECTOR;
 
-/*    seL4_TCB_WriteRegisters(seL4_TCB service, seL4_Bool resume_target, seL4_Uint8 arch_flags, seL4_Word count, seL4_UserContext *regs)
-      count: number of registers to transfer. IP is first, SP is second.
-*/
+    /* actually write the TCB registers. */
     error = seL4_TCB_WriteRegisters(tcb_object.cptr, 0, 0, regs_size, &regs);
     assert(error == 0);
 
-    /* start it running */
+    /* start the new thread running */
     error = seL4_TCB_Resume(tcb_object.cptr);
     assert(error == 0);
 
+    /* we are done, say hello */
     printf("main: hello world\n");
- 
+
+    /*
+     * now send a message to the new thread, and wait for a reply
+     */
+
     seL4_Word msg;
-    seL4_MessageInfo_t tag = seL4_MessageInfo_new(0, 0, 0, 1);
+    seL4_MessageInfo_t tag;
+
+    /* set the data to send. We send it in the first message register */
+    tag = seL4_MessageInfo_new(0, 0, 0, 1);
     seL4_SetMR(0, MSG_DATA);
 
+    /* send and wait for a reply */
     tag = seL4_Call(ep_cap_path.capPtr, tag);
 
+    /* check that we got the expected repy */
     assert(seL4_MessageInfo_get_length(tag) == 1);
     msg = seL4_GetMR(0);
     assert(msg == ~MSG_DATA);
