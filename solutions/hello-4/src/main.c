2b5d0e909c02a6c764b09f2114f72dfe94ee4cd0
Ihor Kuz <ihor.kuz@nicta.com.au>
2015-08-12 21:50:32 -0400

    Cleaned up hello-4 app.

--- solutions/hello-4/src/main.c
+++ solutions/hello-4/src/main.c
@@ -9,7 +9,7 @@
  */
 
 /*
- * Part 4: create a new process
+ * seL4 tutorial part 4: create a new process and IPC with it
  */
 
 
@@ -25,8 +25,6 @@
 #include <simple-default/simple-default.h>
 
 #include <vka/object.h>
-//#include <vka/capops.h>
-//#include <vka/vka.h>
 
 #include <allocman/allocman.h>
 #include <allocman/bootstrap.h>
@@ -38,53 +36,43 @@
 #include <sel4utils/mapping.h>
 #include <sel4utils/process.h>
 
-#define IPCBUF_FRAME_SIZE_BITS 12
+/* constants */
+#define EP_BADGE 0x61 // arbitrary (but unique) number for a badge
+#define MSG_DATA 0x6161 // arbitrary data to send
 
-#define EP_BADGE 0x61
-#define MSG_DATA 0x6161
-
-#define APP_PRIORITY 255
+#define APP_PRIORITY seL4_MaxPrio
 #define APP_IMAGE_NAME "hello-4-app"
 
+/* global environment variables */
 seL4_BootInfo *info;
 simple_t simple;
 vka_t vka;
 allocman_t *allocman;
 vspace_t vspace;
 
-/* dimensions of virtual memory for the allocator to use */
-#define ALLOCATOR_VIRTUAL_POOL_SIZE ((1 << seL4_PageBits) * 100)
-
 /* static memory for the allocator to bootstrap with */
 #define ALLOCATOR_STATIC_POOL_SIZE ((1 << seL4_PageBits) * 10)
 UNUSED static char allocator_mem_pool[ALLOCATOR_STATIC_POOL_SIZE];
 
+/* dimensions of virtual memory for the allocator to use */
+#define ALLOCATOR_VIRTUAL_POOL_SIZE ((1 << seL4_PageBits) * 100)
+
 /* static memory for virtual memory bootstrapping */
 UNUSED static sel4utils_alloc_data_t data;
 
+/* stack for the new thread */
 #define THREAD_2_STACK_SIZE 4096
 UNUSED static int thread_2_stack[THREAD_2_STACK_SIZE];
 
-void abort(void) {
-    while (1);
-}
-
-void __arch_putchar(int c) {
-#ifdef CONFIG_DEBUG_BUILD
-    seL4_DebugPutChar(c);
-#endif
-}
-
+/* convenience function */
+extern void name_thread(seL4_CPtr tcb, char *name);
 
 int main(void)
 {
     UNUSED int error;
-    UNUSED void *ipcbuf_addr;
-    UNUSED seL4_CPtr ipc_frame;
 
-#ifdef SEL4_DEBUG_KERNEL
-    seL4_DebugNameThread(seL4_CapInitThreadTCB, "hello-4");
-#endif
+    /* give us a name: useful for debugging if the thread faults */
+    name_thread(seL4_CapInitThreadTCB, "hello-4");
 
     /* get boot info */
     info = seL4_GetBootInfo();
@@ -92,93 +80,83 @@ int main(void)
     /* init simple */
     simple_default_init_bootinfo(&simple, info);
 
-    /* Print bootinfo and other info about simple */
+    /* print out bootinfo and other info about simple */
     simple_print(&simple);
 
     /* create an allocator */
-    allocman = bootstrap_use_current_simple(&simple, ALLOCATOR_STATIC_POOL_SIZE, allocator_mem_pool);
+    allocman = bootstrap_use_current_simple(&simple, ALLOCATOR_STATIC_POOL_SIZE,
+        allocator_mem_pool);
     assert(allocman);
 
     /* create a vka (interface for interacting with the underlying allocator) */
     allocman_make_vka(&vka, allocman);
 
-    /* create a vspace */
+    /* create a vspace object to manage our vspace */
     error = sel4utils_bootstrap_vspace_with_bootinfo_leaky(&vspace,
-            &data, simple_get_pd(&simple), &vka, info);
+        &data, simple_get_pd(&simple), &vka, info);
 
     /* fill the allocator with virtual memory */
     void *vaddr;
     UNUSED reservation_t virtual_reservation;
     virtual_reservation = vspace_reserve_range(&vspace,
-                 ALLOCATOR_VIRTUAL_POOL_SIZE, seL4_AllRights, 1, &vaddr);
+        ALLOCATOR_VIRTUAL_POOL_SIZE, seL4_AllRights, 1, &vaddr);
     assert(virtual_reservation.res);
     bootstrap_configure_virtual_pool(allocman, vaddr,
-                 ALLOCATOR_VIRTUAL_POOL_SIZE, simple_get_pd(&simple));
-
-
-    /* find image code with cpio */
-    // TODO
+        ALLOCATOR_VIRTUAL_POOL_SIZE, simple_get_pd(&simple));
 
     /* use sel4utils to make a new process*/
     sel4utils_process_t new_process;
     error = sel4utils_configure_process(&new_process, &vka, &vspace,
-            APP_PRIORITY, APP_IMAGE_NAME);
+        APP_PRIORITY, APP_IMAGE_NAME);
     assert(error == 0);
 
-#ifdef SEL4_DEBUG_KERNEL
-    seL4_DebugNameThread(new_process.thread.tcb.cptr, APP_IMAGE_NAME);
-#endif
+    /* give the new process's thread a name */
+    name_thread(new_process.thread.tcb.cptr, "hello-4: process_2");
 
-    /* create new EP */
-    vka_object_t ep_object;
-    ep_object.cptr = 0;
-
+    /* create an endpoint */
+    vka_object_t ep_object = {0};
     error = vka_alloc_endpoint(&vka, &ep_object);
     assert(error == 0);
 
+    /* make a badged enpoint in the new process's cspace.
+     * This copy will be used to send an IPC to the original cap
+     */
     cspacepath_t ep_cap_path;
+    seL4_CPtr new_ep_cap;
     vka_cspace_make_path(&vka, ep_object.cptr, &ep_cap_path);
-    
-    seL4_CPtr new_ep_cap; 
-    new_ep_cap = sel4utils_mint_cap_to_process(&new_process, ep_cap_path, seL4_AllRights, seL4_CapData_Badge_new(EP_BADGE));
+    new_ep_cap = sel4utils_mint_cap_to_process(&new_process, ep_cap_path,
+        seL4_AllRights, seL4_CapData_Badge_new(EP_BADGE));
     assert(new_ep_cap != 0);
-
-    printf("main: new_ep_cap: %#x\n", new_ep_cap);
 
     /* spawn the process */
     error = sel4utils_spawn_process_v(&new_process, &vka, &vspace, 0, NULL, 1);
     assert(error == 0);
 
-#if 0
-    /* send message on EP */
-    seL4_Word msg;
-    seL4_MessageInfo_t tag = seL4_MessageInfo_new(0, 0, 0, 1);
-    seL4_SetMR(0, MSG_DATA);
+    /* we are done, say hello*/
+    printf("main: hello world\n");
 
-    tag = seL4_Call(ep_cap_path.capPtr, tag);
+    /*
+     * now wait for a message from the new process, then send a reply back
+     */
 
-    assert(seL4_MessageInfo_get_length(tag) == 1);
-    msg = seL4_GetMR(0);
-    assert(msg == ~MSG_DATA);
-
-    printf("main: got a reply: %#x\n", msg);
-#endif
     seL4_Word sender_badge;
     seL4_MessageInfo_t tag;
     seL4_Word msg;
 
+    /* wait for a message */
     tag = seL4_Wait(ep_cap_path.capPtr, &sender_badge);
-    printf("got a message from sender badge: %#x\n", sender_badge);
-    assert(sender_badge == EP_BADGE);
 
+    /* make sure it is what we expected */
+    assert(sender_badge == EP_BADGE);
     assert(seL4_MessageInfo_get_length(tag) == 1);
+
+    /* get the message stored in the first message register */
     msg = seL4_GetMR(0);
     printf("main: got a message %#x from %#x\n", msg, sender_badge);
 
+    /* modify the message and send it back */
     seL4_SetMR(0, ~msg);
     seL4_ReplyWait(ep_cap_path.capPtr, tag, &sender_badge);
-
-    printf("main: hello world\n");
 
     return 0;
 }
