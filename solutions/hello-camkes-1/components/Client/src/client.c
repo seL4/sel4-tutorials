2ac5094ab7f5a1f5378e82c7918c59334cdce4af
Ihor Kuz <ihor.kuz@nicta.com.au>
2015-08-13 07:57:50 -0400

    Cleaned up CAmkES tutorial 1.

--- solutions/hello-camkes-1/components/Client/src/client.c
+++ solutions/hello-camkes-1/components/Client/src/client.c
@@ -8,14 +8,21 @@
  * @TAG(NICTA_BSD)
  */
 
+/*
+ * CAmkES tutorial part 1: components with RPC. Client part.
+ */
+
 #include <stdio.h>
 
+/* generated header for our component */
 #include <camkes.h>
 
+/* run the control thread */
 int run(void) {
     printf("Starting the client\n");
     printf("-------------------\n");
 
+    /* invoke the RPC function */
     char *shello = "hello world";
     hello_say_hello(shello);
 
--- solutions/hello-camkes-1/components/Echo/src/echo.c
+++ solutions/hello-camkes-1/components/Echo/src/echo.c
@@ -8,10 +8,17 @@
  * @TAG(NICTA_BSD)
  */
 
+/*
+ * CAmkES tutorial part 1: components with RPC. Server part.
+ */
+
+
 #include <stdio.h>
 
+/* generated header for our component */
 #include <camkes.h>
 
+/* implement the RPC function. */
 void hello_say_hello(const char *str) {
     printf("Component %s saying: %s\n", get_instance_name(), str);
 }
