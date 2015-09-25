6248cef50d1547408960971dbb155f500bc34983
Ihor Kuz <ihor.kuz@nicta.com.au>
2015-09-25 18:25:40 +1000

    Update hello-camkes-1:  Update comments to provide more information and link to documentation.

--- apps/hello-camkes-1/components/Echo/src/echo.c
+++ apps/hello-camkes-1/components/Echo/src/echo.c
@@ -19,3 +19,13 @@
 #include <camkes.h>
 
 /* TODO: implement the RPC function. */
+/* hint 1: the name of the function to implement is a composition of an interface name and a function name:
+ * i.e.: <interface>_<function>
+ * hint 2: the interfaces available are defined by the component, e.g. in components/Echo/Echo.camkes
+ * hint 3: the function name is defined by the interface definition, e.g. in interfaces/HelloSimple.idl4
+ * hint 4: so the function would be: hello_say_hello() 
+ * hint 5: the CAmkES 'string' type maps to 'const char *' in C
+ * hint 6: make the function print out a mesage using printf
+ * hint 7: look at https://github.com/seL4/camkes-tool/blob/master/docs/index.md#creating-an-application
+ */
+
--- solutions/hello-camkes-1/components/Echo/src/echo.c
+++ solutions/hello-camkes-1/components/Echo/src/echo.c
@@ -18,7 +18,17 @@
 /* generated header for our component */
 #include <camkes.h>
 
-/* implement the RPC function. */
+/* TODO: implement the RPC function. */
+/* hint 1: the name of the function to implement is a composition of an interface name and a function name:
+ * i.e.: <interface>_<function>
+ * hint 2: the interfaces available are defined by the component, e.g. in components/Echo/Echo.camkes
+ * hint 3: the function name is defined by the interface definition, e.g. in interfaces/HelloSimple.idl4
+ * hint 4: so the function would be: hello_say_hello() 
+ * hint 5: the CAmkES 'string' type maps to 'const char *' in C
+ * hint 6: make the function print out a mesage using printf
+ * hint 7: look at https://github.com/seL4/camkes-tool/blob/master/docs/index.md#creating-an-application
+ */
 void hello_say_hello(const char *str) {
     printf("Component %s saying: %s\n", get_instance_name(), str);
 }
+
