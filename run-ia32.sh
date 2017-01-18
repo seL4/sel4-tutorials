97b199ad1b1f1720aebfffe19274486d600e4aec
Stephen Sherratt <Stephen.Sherratt@data61.csiro.au>
2017-01-18 16:26:43 +1100

    Running qemu with -cpu Haswell

--- run-ia32.sh
+++ run-ia32.sh
@@ -8,9 +8,9 @@ make ia32_${APP}_defconfig
 make silentoldconfig
 make
 if [[ ${APP} =~ .*camkes.* ]]; then
-    qemu-system-i386 -nographic -m 512   -kernel images/kernel-ia32-pc99   -initrd images/capdl-loader-experimental-image-ia32-pc99 
+    qemu-system-i386 -nographic -m 512  -cpu Haswell  -kernel images/kernel-ia32-pc99   -initrd images/capdl-loader-experimental-image-ia32-pc99
 else
-    qemu-system-i386 -nographic -m 512   -kernel images/kernel-ia32-pc99   -initrd images/${APP}-image-ia32-pc99 
+    qemu-system-i386 -nographic -m 512  -cpu Haswell  -kernel images/kernel-ia32-pc99   -initrd images/${APP}-image-ia32-pc99
 fi
 
 
