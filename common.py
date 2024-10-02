f5780776683185ea062932a4878db430fd201be2
TunaCici <36923239+TunaCici@users.noreply.github.com>
2024-10-02 19:42:06 +0300

    tools: handle different python sh versions
    - ensure that 'RunningCommand' object is returned by checking
      the version of the 'sh' module.
    - use '_return_cmd=True' for sh versions 2.x.
    
    Signed-off-by: Tuna Cici <tunacici7@gmail.com>

--- common.py
+++ common.py
@@ -66,7 +66,14 @@ def _init_build_directory(config, initialised, directory, tute_directory, output
         tute_dir = "-DTUTORIAL_DIR=" + os.path.basename(tute_directory)
         args = ['-G', 'Ninja'] + config_dict[config] + [tute_dir] + \
             ["-C", "../projects/sel4-tutorials/settings.cmake"]
-    return sh.cmake(args + [tute_directory], _cwd=directory, _out=output, _err=output)
+    result = None
+    if sh.__version__.startswith("1"):
+        result = sh.cmake(args + [tute_directory], _cwd=directory, _out=output,
+                          _err=output)
+    else:
+        result = sh.cmake(args + [tute_directory], _cwd=directory, _out=output,
+                          _err=output, _return_cmd=True)
+    return result
 
 
 def _init_tute_directory(config, tut, solution, task, directory, output=None):
